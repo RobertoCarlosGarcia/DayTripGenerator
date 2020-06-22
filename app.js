07d1f42b055714c8f475c3ecdba23d18e88ca903
unknown <RobertCG47@outlook.com>
2020-06-22 18:57:19 -0500

    Added destinations array and selected a destination randomly.

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let dayTrip = ("Houston, Las Vegas, San Diego");
+
+    function shuffleArray(dayTrip){
+
+        let Houston = 0 
+        let LasVegas = 1 
+        let SanDiego = 2 
+        return Math.floor(math.Random() * 3);
+    }
+
+        console.log(itworks);
