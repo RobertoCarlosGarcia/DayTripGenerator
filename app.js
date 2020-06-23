1b2e626abee08d3f3b3364f7528e107a6c65fe49
unknown <RobertCG47@outlook.com>
2020-06-22 20:38:45 -0500

    Generated random mode of transportation with the generateRandomTrip function.

--- app.js
+++ app.js
@@ -2,14 +2,27 @@
 
 let destinations = ["San Francisco", "Texas", "Florida", "New York"];
 let restaurants = ["Outback", "Roadhouse", "Olive Garden", "Perrys"];
+let transportations = ["Train", "Car", "Airplane", "Subway"];
+
+
 
 function generateRandomTrip(){
     let randomIndex = Math.floor(Math.random() * Math.floor(destinations.length)); 
     let destination = destinations[randomIndex];
     console.log(destination);
     
-    randomIndex = Math.floor(Math.random() * Math.floor(restaurants.length));
+randomIndex = Math.floor(Math.random() * Math.floor(restaurants.length));
      let restaurant = restaurants[randomIndex];
      console.log(restaurant);
+
+    randomIndex = Math.floor(Math.random() * Math.floor(transportations.length));
+        let transportation = transportations[randomIndex];
+        console.log(transportation);
+
+    
+
+
 }
-  generateRandomTrip();
+  
+
+generateRandomTrip();
