6aff1e74b2186c640024353fcd9b5a69a8a0da19
unknown <RobertCG47@outlook.com>
2020-06-22 19:13:34 -0500

    Generated random restaurant with the generateRandomTrip function.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 "use strict";
 
 let destinations = ["San Francisco", "Texas", "Florida", "New York"];
+let restaurants = ["Outback", "Roadhouse", "Olive Garden", "Perrys"];
 
 function generateRandomTrip(){
     let randomIndex = Math.floor(Math.random() * Math.floor(destinations.length)); 
     let destination = destinations[randomIndex];
     console.log(destination);
-     randomIndex = Math.floor(Math.random() * Math.floor(destinations.length));
+    
+    randomIndex = Math.floor(Math.random() * Math.floor(restaurants.length));
+     let restaurant = restaurants[randomIndex];
+     console.log(restaurant);
 }
   generateRandomTrip();
